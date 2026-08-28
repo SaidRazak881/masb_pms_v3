b22bee7dfbeffe3188717410ceb32116f0cbb476
SaidRazak881 <saidrazak881@gmail.com>
2026-08-28 18:36:53 +0800

    chore: add TypeScript and shadcn UI foundations

---
+++ components/ui/card.tsx
@@ -0,0 +1,5 @@
+import { cn } from "@/lib/utils";
+export function Card({className,...props}:React.HTMLAttributes<HTMLDivElement>){return <div className={cn("rounded-xl border bg-white shadow-sm",className)} {...props}/>}
+export function CardHeader({className,...props}:React.HTMLAttributes<HTMLDivElement>){return <div className={cn("p-5",className)} {...props}/>}
+export function CardTitle({className,...props}:React.HTMLAttributes<HTMLHeadingElement>){return <h3 className={cn("font-bold",className)} {...props}/>}
+export function CardContent({className,...props}:React.HTMLAttributes<HTMLDivElement>){return <div className={cn("p-5 pt-0",className)} {...props}/>}

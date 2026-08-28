a40259e5159c3b89974ab92b1483631b1276c3bf
SaidRazak881 <saidrazak881@gmail.com>
2026-08-28 18:34:30 +0800

    feat: add Next.js root layout

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'MIMOS Academy PMS',description:'Sistem Pengurusan R1/R2/R3 MIMOS Academy'}
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang='ms'><body>{children}</body></html>}
